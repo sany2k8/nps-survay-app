ab72e751b4e1e7c30e5092e2c3e523c500434463
Md. Sany Ahmed <sany2k8@gmail.com>
2017-03-14 08:25:30 +0600

    fix height for index view

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,7 +3,13 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
+<div class="grid_10">
 
 <!-- Demo Scripts (remove if not needed) -->
 <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/demo/demo.dashboard.js',CClientScript::POS_HEAD); ?>
+</div>
+<style>
+    div#mws-navigation {
+        height: 750px;
+    }
+</style>
